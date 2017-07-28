9145b1b65b81b564f419ae5bdb5baa1dccebb95d
marr6h <marr6h@yahoo.co.jp>
2017-07-28 19:19:28 +0900

    Add search logic to search action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
     def search
+        if params[:email]
+            @users = User.where(email: params[:email])
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :posts
   get 'users/search', to: 'users#search'
+  post 'users/search', to: 'users#search'
 end
