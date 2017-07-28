995e0fc10c81171ff4efebfd427d25d611dd5829
marr6h <marr6h@yahoo.co.jp>
2017-07-28 19:46:39 +0900

    Add RESTful routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+    def index
+        @user = current_user
+    end
+    def edit
+        @user = current_user
+    end
     def search
         if params[:email]
             @users = User.where(email: params[:email])
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :posts
   get 'users/search', to: 'users#search'
   post 'users/search', to: 'users#search'
+  resources :users
 end
