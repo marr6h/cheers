087f3df04053c9b4b5cc6d5826053e055dc50037
marr6h <marr6h@yahoo.co.jp>
2017-08-21 15:24:27 +0900

    Add unittest for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
+    DRINKING_HIGH = 3
+    DRINKING_MIDDLE = 2
+    DRINKING_LOW = 1
+
     belongs_to :user
+
+    validates :drinking_status, presence: true, inclusion: {in: [DRINKING_HIGH, DRINKING_MIDDLE, DRINKING_LOW]}
 end
