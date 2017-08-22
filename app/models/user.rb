4c04d8c417308c92909e5fbfcdbdad8b0e9a0777
marr6h <marr6h@yahoo.co.jp>
2017-08-22 11:54:18 +0900

    Relate user to user by relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,30 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                    foreign_key: "follower_id",
+                    dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                    foreign_key: "followed_id",
+                    dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   mount_uploader :image, AvatarUploader
+  
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+  
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
 end
