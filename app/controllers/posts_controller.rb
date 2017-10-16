8e217602bf5555b7ed85f280397f167946a3b24e
konno <marr6h@yahoo.co.jp>
2017-10-16 20:35:50 +0900

    Create UI for editing post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,13 @@ class PostsController < ApplicationController
         @post = Post.new
     end
 
+    def edit
+        @post = Post.find(params[:id])
+        render :action => 'new'
+    end
+
     def create
-        @post = Post.new(user_params)
+        @post = Post.new(post_params)
         @post.user_id = current_user.id
         if @post.save
             redirect_to :root
@@ -14,12 +19,21 @@ class PostsController < ApplicationController
         end
     end
 
+    def update
+        @post = Post.find(params[:id])
+        if @post.update_attributes(post_params)
+            redirect_to :root
+        else
+            render 'edit'
+        end
+    end
+
     def show
         @post = Post.find(params[:id])
     end
 
     private
-      def user_params
+      def post_params
         params.require(:post).permit(:drinking_status, :datetime, :location, :remarks)
       end
 end
