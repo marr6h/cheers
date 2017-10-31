efe80de80599792b44154ca02189c6d2f555c959
Large T <marr6h@yahoo.co.jp>
2017-10-31 12:57:58 +0900

    Modify test code without entry model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  
+
   def setup
     @user = users (:john)
   end
@@ -13,6 +13,8 @@ class PostTest < ActiveSupport::TestCase
   test 'valid drinking_status' do
     post = Post.new
     post.user_id = @user.id
+    now = Time.now
+    post.datetime = Time.new(now.year, now.month, now.day, 19, 0, 0)
     post.drinking_status = Post::DRINKING_HIGH
     assert post.save
     post.drinking_status = Post::DRINKING_MIDDLE
@@ -24,6 +26,7 @@ class PostTest < ActiveSupport::TestCase
   test 'invalid drinking_status' do
     post = Post.new
     post.user_id = @user.id
+    post.datetime = Time.now
     # empty drinking_status
     assert_equal false, post.save
     # does not definied drinking_status
