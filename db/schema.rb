8b65154465bcd0dc20dc1401837510011458712f
Large T <marr6h@yahoo.co.jp>
2017-10-31 12:19:05 +0900

    Apply entry model migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822020616) do
+ActiveRecord::Schema.define(version: 20171031030141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entries", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "entry_to"
+    t.integer  "entry_from"
+    t.integer  "cheer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.datetime "datetime"
