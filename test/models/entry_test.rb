9d6ad011eeb3f995bacb5398a189ee5fc15a8f8a
Large T <marr6h@yahoo.co.jp>
2017-10-31 12:44:45 +0900

    Implement testing to entry model

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -4,4 +4,17 @@ class EntryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should require a post_id" do
+      entry = Entry.new
+      entry.entry_from = 0
+      assert !entry.save
+  end
+
+  test "Should require a entry_from" do
+      entry = Entry.new
+      entry.post_id = 0
+      assert !entry.save
+  end
+
 end
