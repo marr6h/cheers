caf25132eea5b097fbe3b359f17a5ec0e7f14a9f
Large T <marr6h@yahoo.co.jp>
2018-02-09 13:03:47 +0900

    Refactoring unit test of relationship model

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -4,25 +4,25 @@ class RelationshipTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  
+
   def setup
     @relationship = Relationship.new(follower_id: users(:john).id, followed_id: users(:paul).id)
   end
-  
+
   test "should be valid" do
     assert @relationship.valid?
   end
-  
+
   test "should require a follower_id" do
     @relationship.follower_id = nil
-    assert_not @relationship.valid?
+    assert @relationship.invalid?
   end
-  
+
   test "should require a followed_id" do
     @relationship.followed_id = nil
-    assert_not @relationship.valid?
+    assert @relationship.invalid?
   end
-  
+
   test "should follow and unfollow a user" do
     john = users(:john)
     paul = users(:paul)
