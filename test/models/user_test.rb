a5d0f467042ab86e235b1ca3f344a10b4f442f79
Large T <marr6h@yahoo.co.jp>
2018-02-22 12:56:30 +0900

    Improve testing code

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,7 +6,8 @@ class UserTest < ActiveSupport::TestCase
         other_user = users(:paul)
         user.follow(other_user)
         posts = user.aggregate_post
-        assert_equal posts.length, 0
+        assert_equal 0, posts.length
+        assert_equal({}, posts)
     end
 
     test 'user aggregates 1 posts' do
@@ -15,11 +16,13 @@ class UserTest < ActiveSupport::TestCase
         user.follow(other_user)
         post = Post.new
         post.user_id = other_user.id
-        post.datetime = Time.new(Time.now.year, Time.now.month, Time.now.day, 19, 0, 0)
+        post.datetime = Time.new(Time.now.year, Time.now.month, Time.now.day, 19, 0, 0, "+09:00")
         post.drinking_status = Post::DRINKING_HIGH
         post.save
         posts = user.aggregate_post
-        assert_equal posts.length, 1
+        assert_equal 1, posts.length
+        assert posts.include?('19')
+        assert_equal true, posts['19'].present?
     end
 
 end
